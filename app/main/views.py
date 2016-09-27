6428ee8c0a4dde1b81d57b3286023a468180c0e3
shepherd <heimd4ll@yeah.net>
2016-09-27 15:59:53 +0800

    front page version 1.0

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,22 @@
 from . import main
+from flask import render_template
 
 
 @main.route('/', methods=['GET'])
 def index():
-    return '<h1>hello,world</h1>'
+    return render_template('index.html')
+
+
+@main.route('/ladder', methods=['GET'])
+def ladder():
+    return render_template('ladder.html')
+
+
+@main.route('/query', methods=['GET'])
+def query():
+    return render_template('query.html')
+
+
+@main.route('/sponsor', methods=['GET'])
+def sponsor():
+    return render_template('sponsor.html')
