a53ace6fd1808d68a7852517e7164ad97bd8f3ca
shepherd <heimd4ll@yeah.net>
2016-09-29 12:54:02 +0800

    add queries.py

---
+++ app/queries.py
@@ -0,0 +1,17 @@
+
+def get_top15():
+    data = [
+        {
+            'rank': 1,
+            'name': 'xiaohu',
+            'id': 'leap',
+            'lp': 1300
+        },
+        {
+            'rank': 2,
+            'name': 'faker',
+            'id': 'hideonbush',
+            'lp': 1250
+        }
+    ]
+    return data
