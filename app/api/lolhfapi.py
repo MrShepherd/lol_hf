8d8cf2b6d8bd5b7cf90121f4a991be67158cc308
shepherd <heimd4ll@yeah.net>
2016-11-01 10:19:14 +0800

    modify data model

--- app/api/lolhfapi.py
+++ app/api/lolhfapi.py
@@ -5,22 +5,11 @@ from .. import queries
 
 @api.route('/api/top15/')
 def get_top15():
-    data = [
-        {
-            'rank': 1,
-            'name': 'xiaohu',
-            'lp': 1300
-        },
-        {
-            'rank': 2,
-            'name': 'faker',
-            'lp': 1250
-        }
-    ]
+    data = [{'rank': 1, 'name': 'xiaohu', 'lp': 1300}, {'rank': 2, 'name': 'faker', 'lp': 1250}]
     return jsonify({'data': data})
 
 
 @api.route('/api/shangdandata/')
 def get_shangdan_data():
-    data = queries.get_place_data('shangdan')
+    data = queries.get_place_data()
     return jsonify({'data': data})
